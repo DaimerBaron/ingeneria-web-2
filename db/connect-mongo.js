a4ccec790a09c69cd26eb3e815766c48e9dbd2a2
DaimerBaron <daimer.baron@est.iudigital.edu.co>
2025-02-21 20:55:40 -0500

    Cambio el tipo de importacion y agrego la carpeta controller

--- db/connect-mongo.js
+++ db/connect-mongo.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 const getConnetion = async () => {
   try {
     const url =
-      "mongodb+srv://daimerbaron:tHApkqRlX8W9FPL3@cluster0.a0kcq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+      "mongodb+srv://daimerbaron:9SrbrOM8793P9jwb@ingeneriaweb2.ee0fz.mongodb.net/?retryWrites=true&w=majority&appName=ingeneriaWeb2";
 
     await mongoose.connect(url);
     console.log("Connected to MongoDB");
