0676467013f8b5719ec321b83980600c774805c5
DaimerBaron <daimer.baron@est.iudigital.edu.co>
2025-02-24 05:20:08 -0500

    CRUD del modulo media completado

--- controllers/media.js
+++ controllers/media.js
@@ -6,11 +6,20 @@ class MediaController {
     }
     async getMedia(req, res) {
         try {
-            const media = await Media.find(); //para obtener todos lo registros
-            console.log(media);
-            
+            const media = await Media.find();
+            res.status(200).json(media);
         } catch (error) {
             res.status(500).send(error);
+        }
+    }
+
+    async getOneMedia (req, res){
+        try {
+            const {id} = req.params;
+            const media = await Media.findById(id);
+            res.status(200).json(media)
+        } catch (error) {
+            res.status(500).send(error)
         }
     }
 
@@ -27,14 +36,22 @@ class MediaController {
     }
     async updateMedia(req, res) {
         try {
-            res.status(201).json({status: 'update-okay'});
+            const { id } = req.params;
+            const updatedMedia = await Media.findByIdAndUpdate(id, { ...req.body }, { new: true });
+            if (!updatedMedia) {
+                return res.status(404).send('Media not found');
+            }   
+            res.status(201).json(updatedMedia);
         } catch (error) {
             res.status(500).send(error);
         }
     }
     async deleteMedia(req, res) {
+        const { id } = req.params;
+        const media = await Media.findByIdAndDelete(id);
         try {
-            res.status(201).json({status: 'delete-okay'});
+          
+            res.status(201).json(media);
         } catch (error) {
             res.status(500).send(error);
         }
--- routes/media.js
+++ routes/media.js
@@ -4,6 +4,7 @@ import MediaController from "../controllers/media.js";
 const router = express.Router();
 
 router.get('/',MediaController.getMedia);
+router.get('/:id',MediaController.getOneMedia)
 router.post('/',MediaController.createMedia);
 router.put('/:id',MediaController.updateMedia);
 router.delete('/:id',MediaController.deleteMedia);
