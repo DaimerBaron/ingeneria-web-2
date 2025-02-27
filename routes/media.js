b268cef5d2ec9c1bb5375851bd345ff6eb734d57
AndreaAramburo93 <169169868+AndreaAramburo93@users.noreply.github.com>
2025-02-27 11:11:32 -0500

    actualizacion en codigo repetido de genre

--- routes/media.js
+++ routes/media.js
@@ -2,17 +2,7 @@ import express from "express";
 import MediaController from "../controllers/media.js";
 import {validationResult,check} from "express-validator";
 
-const validateType = [
-    check("name", "invalid.name").not().isEmpty(),
-    check("description","invalid.description").not().isEmpty(),
-    function (req, res, next){
-        const errors = validationResult (req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json ({message: errors.array()});
-        }
-        next();
-    }
-]
+
 
 const router = express.Router();
 
--- routes/type.js
+++ routes/type.js
@@ -3,17 +3,7 @@ import express from 'express';
 import { validationResult, check } from 'express-validator';
 
 
-const validateType = [
-    check('name', 'invalid.name').not().isEmpty(),
-    check('description', 'invalid.description').not().isEmpty(),
-    function (req, res, next) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-        return res.status(400).json({ message: errors.array() });
-        }
-        next();
-    },
-]
+
 
 const router = express.Router();
 const validateType = [
