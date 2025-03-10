545e33f6f9e0b04c3bdf30450a302643b6d5fe7b
DaimerBaron <daimer.baron@est.iudigital.edu.co>
2025-03-10 17:42:27 -0500

    new dependency cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import mediaRoutes from './routes/media.js';
 import genreRoutes from './routes/genre.js';
 import typeRoutes from './routes/type.js';
@@ -6,9 +7,12 @@ import getConnetion from './db/connect-mongo.js';
 import directorRoutes from './routes/director.js';
 import producerRoutes from './routes/producer.js';
 
+
+
 const app = express();
 const port = 5100;
 
+app.use(cors());
 app.use(express.json());
 app.use('/media',mediaRoutes)
 app.use('/genre',genreRoutes)
